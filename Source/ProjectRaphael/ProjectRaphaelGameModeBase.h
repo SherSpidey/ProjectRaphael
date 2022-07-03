ec11914cb7734b66e32396ea3e4ae238536ec79d
SherSpidey <sherspidey@163.com>
2022-07-03 17:24:10 +0800

    First Update

---
+++ Source/ProjectRaphael/ProjectRaphaelGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ProjectRaphaelGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTRAPHAEL_API AProjectRaphaelGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
